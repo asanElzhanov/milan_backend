4bcd6cd9a53b6fc65f92763b3dceea0d752f04d3
tamir <your.email@example.com>
2026-07-30 11:25:48 +0500

    celery version adapting

--- config/celery.py
+++ config/celery.py
@@ -6,7 +6,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 app = Celery('shop')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
-# Use django-celery-beat's DatabaseScheduler
-from celery.schedules import schedule
-from django_celery_beat.schedulers import DatabaseScheduler
